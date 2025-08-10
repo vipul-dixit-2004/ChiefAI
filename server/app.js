3f6e0a6346d74ff77fe0b6483b30812e6f66ced4
vipul-dixit-2004 <dixit.vipul0511@gmail.com>
2025-08-10 22:00:21 +0530

    server side cors setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const { connectToDb } = require("./connectDb");
 const userRoute = require('./router/user');
@@ -13,8 +13,10 @@ connectToDb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://chief-ai-ten.vercel.app"],
-  credentials: true,
+  origin: "https://chief-ai-ten.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 app.use("/user", userRoute);//routes for user
 app.use("/recipe", recipeRoute);
