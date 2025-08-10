704b433b555be75c11a28bbc10f02269721dca20
vipul-dixit-2004 <dixit.vipul0511@gmail.com>
2025-08-10 21:50:21 +0530

    cors error resolved

--- server/app.js
+++ server/app.js
@@ -13,8 +13,8 @@ connectToDb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend origin
-  credentials: true, // allow cookies to be sent
+  origin: ["https://chief-ai-ten.vercel.app"],
+  credentials: true,
 }));
 app.use("/user", userRoute);//routes for user
 app.use("/recipe", recipeRoute);
