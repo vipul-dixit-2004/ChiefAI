66665541b8043557e2fee48d82800eda60ebc59d
vipul-dixit-2004 <dixit.vipul0511@gmail.com>
2025-08-10 21:46:52 +0530

    main is the production branch now

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
-    // baseURL: 'https://chiefai.onrender.com',
+    baseURL: 'https://chiefai.onrender.com',
     withCredentials: true
 });
 
